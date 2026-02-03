f2cc52430e3ff4db7dd6134203ab6425e676b20e
dixit.thakur <erdixitthakur@gmail.com>
2026-02-03 20:09:07 +0530

    static params added

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,5 +1,10 @@
 import BlogDetailPage from "@/components/BlogDetailPage";
 
+export async function generateStaticParams() {
+    const slugs = ["1", "2"];
+    return slugs.map((slug) => ({ slug }));
+}
+
 export default function Blog() {
     return (
         <BlogDetailPage />
