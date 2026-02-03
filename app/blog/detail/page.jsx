98221d68987117ae9242ee58b9816e9845be258b
dixit.thakur <erdixitthakur@gmail.com>
2026-02-03 20:13:59 +0530

    detail page added

--- app/blog/detail/page.jsx
+++ app/blog/detail/page.jsx
@@ -1,10 +1,5 @@
 import BlogDetailPage from "@/components/BlogDetailPage";
 
-export async function generateStaticParams() {
-    const slugs = ["1", "2"];
-    return slugs.map((slug) => ({ slug }));
-}
-
 export default function Blog() {
     return (
         <BlogDetailPage />
